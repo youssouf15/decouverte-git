8281cddd59ac802d4e341f6b45acb393488f2a76
youssouf ahmed <youssouf.ahmed.ibrahim@gmail.com>
2020-01-27 11:27:47 +0100

    premier module js menu

---
+++ menu.js
@@ -0,0 +1,14 @@
+const menu = `
+    <ul>
+        <li>
+            <a href="index.html">accueil</a>
+        </li>
+        <li>
+            <a href="contact.html">contact</a>
+        </li>
+    </ul>
+
+
+`;
+
+document.getElementById("navigation").innerHTML = menu ;
