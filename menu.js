41542cc283afc0b6b02b2f0036f612aa0a1474d6
youssouf ahmed <youssouf.ahmed.ibrahim@gmail.com>
2020-01-27 12:04:19 +0100

    ajout de la page presentation

--- menu.js
+++ menu.js
@@ -6,6 +6,9 @@ const menu = `
         <li>
             <a href="contact.html">contact</a>
         </li>
+        <li>
+            <a href="presentation.html">contact</a>
+        </li>
     </ul>
 
 
